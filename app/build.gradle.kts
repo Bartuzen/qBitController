621b5dceda36ab344255980075e95f8959550567
Bartu Özen <15032958+Bartuzen@users.noreply.github.com>
2021-11-11 19:28:01 +0300

    Update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,7 +47,7 @@ kapt {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.6.0")
+    implementation("androidx.core:core-ktx:1.7.0")
     implementation("androidx.appcompat:appcompat:1.3.1")
     implementation("com.google.android.material:material:1.4.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.1")
@@ -58,13 +58,13 @@ dependencies {
 
     debugImplementation("com.squareup.leakcanary:leakcanary-android:2.7")
 
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.1-native-mt")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.1-native-mt")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.2")
 
     implementation("androidx.fragment:fragment-ktx:1.3.6")
 
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.3.1")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.3.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.4.0")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.4.0")
 
     implementation("com.google.dagger:hilt-android:2.38.1")
     kapt("com.google.dagger:hilt-compiler:2.38.1")
