040f3b7f795d6f10c86eaa5ed5b4cabe57cab6b4
Bartu Özen <15032958+Bartuzen@users.noreply.github.com>
2021-12-16 13:02:00 +0300

    Remove unused dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,7 @@ android {
         viewBinding = true
         compose = true
     }
+
     composeOptions {
         kotlinCompilerExtensionVersion = "1.1.0-beta04"
     }
@@ -55,7 +56,6 @@ dependencies {
     implementation("androidx.core:core-ktx:1.7.0")
     implementation("androidx.appcompat:appcompat:1.4.0")
     implementation("com.google.android.material:material:1.4.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.2")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
@@ -66,20 +66,10 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.2")
 
-    implementation("androidx.fragment:fragment-ktx:1.4.0")
-
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.4.0")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.4.0")
-
     implementation("com.google.dagger:hilt-android:2.40.2")
     kapt("com.google.dagger:hilt-compiler:2.40.2")
 
     implementation("androidx.datastore:datastore-preferences:1.0.0")
-
-    implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
-
-    implementation("com.hannesdorfmann.fragmentargs:annotation:4.0.0-RC1")
-    kapt("com.hannesdorfmann.fragmentargs:processor:4.0.0-RC1")
 
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-scalars:2.9.0")
