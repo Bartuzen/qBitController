48362960705d4836b3e7c6e3b7e33d23d272fa46
Bartu Özen <15032958+Bartuzen@users.noreply.github.com>
2021-12-16 13:11:17 +0300

    Remove setting theme from App class

--- app/src/main/java/dev/bartuzen/qbitcontroller/App.kt
+++ app/src/main/java/dev/bartuzen/qbitcontroller/App.kt
@@ -1,25 +1,7 @@
 package dev.bartuzen.qbitcontroller
 
 import android.app.Application
-import androidx.appcompat.app.AppCompatDelegate
 import dagger.hilt.android.HiltAndroidApp
-import dev.bartuzen.qbitcontroller.data.SettingsManager
-import dev.bartuzen.qbitcontroller.data.toDelegate
-import kotlinx.coroutines.MainScope
-import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.launch
-import javax.inject.Inject
 
 @HiltAndroidApp
-class App : Application() {
-    @Inject lateinit var settingsManager: SettingsManager
-
-    override fun onCreate() {
-        super.onCreate()
-        MainScope().launch {
-            settingsManager.themeFlow.collectLatest { theme ->
-                AppCompatDelegate.setDefaultNightMode(theme.toDelegate())
-            }
-        }
-    }
-}
+class App : Application()
