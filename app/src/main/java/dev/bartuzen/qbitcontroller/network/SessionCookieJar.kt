68a1488a1fdd1ad43f7c35fdbf88b8a32a95f08f
Bartu Özen <15032958+Bartuzen@users.noreply.github.com>
2022-08-23 10:59:43 +0300

    Fix function signature

--- app/src/main/java/dev/bartuzen/qbitcontroller/network/SessionCookieJar.kt
+++ app/src/main/java/dev/bartuzen/qbitcontroller/network/SessionCookieJar.kt
@@ -6,17 +6,16 @@ import okhttp3.HttpUrl
 
 class SessionCookieJar : CookieJar {
     private var cookies: List<Cookie>? = null
+
     override fun saveFromResponse(url: HttpUrl, cookies: List<Cookie>) {
         if (url.encodedPath().endsWith("login")) {
             this.cookies = cookies
         }
     }
 
-    override fun loadForRequest(url: HttpUrl) =
-        if (!url.encodedPath().endsWith("login") && cookies != null) {
-            cookies
-        } else {
-            emptyList()
-        }
-
+    override fun loadForRequest(url: HttpUrl) = if (!url.encodedPath().endsWith("login")) {
+        cookies?.toList() ?: emptyList()
+    } else {
+        emptyList()
+    }
 }
