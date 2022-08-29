a432ee9e80f7389da5eba49caee59f740412062c
Bartu Özen <15032958+Bartuzen@users.noreply.github.com>
2022-08-29 21:27:53 +0300

    Improve TagDeserializer

--- app/src/main/java/dev/bartuzen/qbitcontroller/model/deserializers/TagDeserializer.kt
+++ app/src/main/java/dev/bartuzen/qbitcontroller/model/deserializers/TagDeserializer.kt
@@ -4,9 +4,8 @@ import com.fasterxml.jackson.core.JsonParser
 import com.fasterxml.jackson.databind.DeserializationContext
 import com.fasterxml.jackson.databind.JsonDeserializer
 
-class TagDeserializer : JsonDeserializer<List<String>?>() {
-    override fun deserialize(parser: JsonParser?, context: DeserializationContext?): List<String>? {
-        val tags = parser?.codec?.readValue(parser, String::class.java) ?: return null
-        return if (tags.isNotEmpty()) tags.split(",") else emptyList()
+class TagDeserializer : JsonDeserializer<List<String>>() {
+    override fun deserialize(parser: JsonParser?, context: DeserializationContext?): List<String> {
+        return parser?.valueAsString?.takeIf { it.isNotEmpty() }?.split(",") ?: emptyList()
     }
 }
