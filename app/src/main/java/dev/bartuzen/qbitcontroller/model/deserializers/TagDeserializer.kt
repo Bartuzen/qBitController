5e63e4d38c873681ac6992f24215536a8f9076af
Bartu Özen <15032958+Bartuzen@users.noreply.github.com>
2022-10-29 00:00:50 +0300

    Fix tags having an extra space behind their names

--- app/src/main/java/dev/bartuzen/qbitcontroller/model/deserializers/TagDeserializer.kt
+++ app/src/main/java/dev/bartuzen/qbitcontroller/model/deserializers/TagDeserializer.kt
@@ -6,6 +6,6 @@ import com.fasterxml.jackson.databind.JsonDeserializer
 
 class TagDeserializer : JsonDeserializer<List<String>>() {
     override fun deserialize(parser: JsonParser?, context: DeserializationContext?): List<String> {
-        return parser?.valueAsString?.takeIf { it.isNotEmpty() }?.split(",") ?: emptyList()
+        return parser?.valueAsString?.takeIf { it.isNotEmpty() }?.split(", ") ?: emptyList()
     }
 }
