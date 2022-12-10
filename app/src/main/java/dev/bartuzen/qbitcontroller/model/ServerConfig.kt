0f15afa7f947d02da5d6f5c9fa82bab3620b4163
Bartu Özen <15032958+Bartuzen@users.noreply.github.com>
2022-12-11 00:33:12 +0300

    Add slash to end of path

--- app/src/main/java/dev/bartuzen/qbitcontroller/model/ServerConfig.kt
+++ app/src/main/java/dev/bartuzen/qbitcontroller/model/ServerConfig.kt
@@ -24,6 +24,9 @@ data class ServerConfig(
             }
             path?.let { path ->
                 append("/$path")
+                if (!path.endsWith("/")) {
+                    append("/")
+                }
             }
         }
 }
