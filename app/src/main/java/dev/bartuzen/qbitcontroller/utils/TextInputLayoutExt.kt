b9412982423cd1663600d757f41e51dfd8ef4e25
Bartu Özen <15032958+Bartuzen@users.noreply.github.com>
2022-12-16 18:12:21 +0300

    Put EditText cursor to end when text there is a default text

--- app/src/main/java/dev/bartuzen/qbitcontroller/utils/TextInputLayoutExt.kt
+++ app/src/main/java/dev/bartuzen/qbitcontroller/utils/TextInputLayoutExt.kt
@@ -3,12 +3,15 @@ package dev.bartuzen.qbitcontroller.utils
 import androidx.annotation.StringRes
 import com.google.android.material.textfield.TextInputLayout
 
-fun TextInputLayout.setTextWithoutAnimation(text: String?) {
+fun TextInputLayout.setTextWithoutAnimation(text: String?, putCursorToEnd: Boolean = true) {
     isHintAnimationEnabled = false
     editText?.setText(text)
+    if (putCursorToEnd) {
+        editText?.setSelection(text?.length ?: 0)
+    }
     isHintAnimationEnabled = true
 }
 
-fun TextInputLayout.setTextWithoutAnimation(@StringRes id: Int) {
-    setTextWithoutAnimation(context.getString(id))
+fun TextInputLayout.setTextWithoutAnimation(@StringRes id: Int, putCursorToEnd: Boolean = true) {
+    setTextWithoutAnimation(context.getString(id), putCursorToEnd)
 }
