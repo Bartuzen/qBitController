d1c3d4081d2557635312ad25542736da746e1206
Bartu Özen <15032958+Bartuzen@users.noreply.github.com>
2022-12-23 04:10:08 +0300

    Fix F-Droid build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "dev.bartuzen.qbitcontroller"
         minSdk = 21
         targetSdk = 33
-        versionCode = 1
-        versionName = "0.1"
+        versionCode = 2
+        versionName = "0.1.1"
     }
 
     buildTypes {
