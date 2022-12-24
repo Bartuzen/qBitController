d7110b57d595827aa32edc499e521d6f3f79abbc
Bartu Özen <15032958+Bartuzen@users.noreply.github.com>
2022-12-24 12:10:22 +0300

    Remove unused property

--- app/src/main/java/dev/bartuzen/qbitcontroller/model/TorrentFile.kt
+++ app/src/main/java/dev/bartuzen/qbitcontroller/model/TorrentFile.kt
@@ -3,9 +3,6 @@ package dev.bartuzen.qbitcontroller.model
 import com.fasterxml.jackson.annotation.JsonProperty
 
 data class TorrentFile(
-    @JsonProperty("index")
-    val index: Int,
-
     @JsonProperty("name")
     val name: String
 )
