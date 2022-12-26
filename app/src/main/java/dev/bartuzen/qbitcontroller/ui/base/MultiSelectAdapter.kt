db2ade4104bb88cea1e5f48da59286f487552f21
Bartu Özen <15032958+Bartuzen@users.noreply.github.com>
2022-12-26 11:12:49 +0300

    Make getKey protected

--- app/src/main/java/dev/bartuzen/qbitcontroller/ui/base/MultiSelectAdapter.kt
+++ app/src/main/java/dev/bartuzen/qbitcontroller/ui/base/MultiSelectAdapter.kt
@@ -7,7 +7,7 @@ import androidx.recyclerview.widget.RecyclerView
 
 abstract class MultiSelectAdapter<T, K, VH : MultiSelectAdapter.ViewHolder<T, K>>(
     diffCallBack: DiffUtil.ItemCallback<T>,
-    val getKey: (item: T) -> K
+    protected val getKey: (item: T) -> K
 ) : ListAdapter<T, VH>(diffCallBack) {
     private val _selectedItems = mutableListOf<K>()
     val selectedItems: List<K> get() = _selectedItems
