b67e6bd12260f4488a1080b10e6b037fa0c605c8
Bartu Özen <15032958+Bartuzen@users.noreply.github.com>
2022-12-28 01:42:20 +0300

    Version 0.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "dev.bartuzen.qbitcontroller"
         minSdk = 21
         targetSdk = 33
-        versionCode = 2
-        versionName = "0.1.1"
+        versionCode = 3
+        versionName = "0.2.0"
     }
 
     buildTypes {
