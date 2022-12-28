50a0ddaf8a12ab1b846af62f98a4973d6cf63e60
Bartu Özen <15032958+Bartuzen@users.noreply.github.com>
2022-12-28 20:02:58 +0300

    Update Kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,7 @@ buildscript {
     }
     dependencies {
         classpath("com.android.tools.build:gradle:7.3.1")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.22")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.0")
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.44.2")
 
         classpath("com.google.gms:google-services:4.3.14")
