8165d0ba1a38eaa7061dbaa32a52a85200fd28c7
Bartu Özen <15032958+Bartuzen@users.noreply.github.com>
2022-12-29 12:26:28 +0300

    Fix a crash on older qBittorrent versions

--- app/src/main/java/dev/bartuzen/qbitcontroller/model/deserializers/TorrentFilePriorityDeserializer.kt
+++ app/src/main/java/dev/bartuzen/qbitcontroller/model/deserializers/TorrentFilePriorityDeserializer.kt
@@ -8,6 +8,10 @@ import dev.bartuzen.qbitcontroller.model.TorrentFilePriority
 class TorrentFilePriorityDeserializer : JsonDeserializer<TorrentFilePriority>() {
     override fun deserialize(parser: JsonParser?, context: DeserializationContext?): TorrentFilePriority {
         val priorityId = parser?.valueAsInt
+        // Seems like older versions of qBittorrent use 4 as normal priority
+        if (priorityId == 4) {
+            return TorrentFilePriority.NORMAL
+        }
         return TorrentFilePriority.values().find { it.id == priorityId }
             ?: throw IllegalArgumentException("Unknown priority: $priorityId")
     }
