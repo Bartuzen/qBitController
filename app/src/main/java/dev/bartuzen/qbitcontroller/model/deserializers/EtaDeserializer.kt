6d0fdac2992f7649ec6ee498a9349d6e7eaf7c69
Bartu Özen <15032958+Bartuzen@users.noreply.github.com>
2022-12-30 10:59:19 +0300

    Return null if ETA value doesn't fit into an Int

--- app/src/main/java/dev/bartuzen/qbitcontroller/model/deserializers/EtaDeserializer.kt
+++ app/src/main/java/dev/bartuzen/qbitcontroller/model/deserializers/EtaDeserializer.kt
@@ -1,11 +1,16 @@
 package dev.bartuzen.qbitcontroller.model.deserializers
 
 import com.fasterxml.jackson.core.JsonParser
+import com.fasterxml.jackson.core.exc.InputCoercionException
 import com.fasterxml.jackson.databind.DeserializationContext
 import com.fasterxml.jackson.databind.JsonDeserializer
 
 class EtaDeserializer : JsonDeserializer<Int?>() {
     override fun deserialize(parser: JsonParser?, context: DeserializationContext?): Int? {
-        return parser?.intValue.takeIf { it != null && (it in 0..8640000) }?.toInt()
+        return try {
+            parser?.intValue.takeIf { it != null && (it in 0..8640000) }?.toInt()
+        } catch (_: InputCoercionException) {
+            null
+        }
     }
 }
