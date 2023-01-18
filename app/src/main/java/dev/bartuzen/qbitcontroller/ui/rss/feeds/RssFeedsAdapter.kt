82fc48e8c006848ab702a79043536dd07789f0dc
Bartu Özen <15032958+Bartuzen@users.noreply.github.com>
2023-01-18 13:16:52 +0300

    Rename item_feed.xml to item_rss_feed.xml

--- app/src/main/java/dev/bartuzen/qbitcontroller/ui/rss/feeds/RssFeedsAdapter.kt
+++ app/src/main/java/dev/bartuzen/qbitcontroller/ui/rss/feeds/RssFeedsAdapter.kt
@@ -5,7 +5,7 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import dev.bartuzen.qbitcontroller.databinding.ItemFeedBinding
+import dev.bartuzen.qbitcontroller.databinding.ItemRssFeedBinding
 import dev.bartuzen.qbitcontroller.model.RssFeed
 
 class RssFeedsAdapter(
@@ -13,7 +13,7 @@ class RssFeedsAdapter(
 ) : ListAdapter<RssFeed, RssFeedsAdapter.ViewHolder>(DiffCallback()) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = ViewHolder(
-        ItemFeedBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        ItemRssFeedBinding.inflate(LayoutInflater.from(parent.context), parent, false)
     )
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
@@ -22,7 +22,7 @@ class RssFeedsAdapter(
         }
     }
 
-    inner class ViewHolder(private val binding: ItemFeedBinding) : RecyclerView.ViewHolder(binding.root) {
+    inner class ViewHolder(private val binding: ItemRssFeedBinding) : RecyclerView.ViewHolder(binding.root) {
         init {
             binding.root.setOnClickListener {
                 if (bindingAdapterPosition != RecyclerView.NO_POSITION) {
