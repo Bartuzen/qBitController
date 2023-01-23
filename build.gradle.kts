91717c6f8db21b11026d627ee29821219abbcbe2
Bartu Özen <15032958+Bartuzen@users.noreply.github.com>
2023-01-24 02:24:30 +0300

    Update Firebase

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,7 +36,7 @@ buildscript {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.0")
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.44.2")
 
-        classpath("com.google.gms:google-services:4.3.14")
+        classpath("com.google.gms:google-services:4.3.15")
         classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.2")
     }
 }
