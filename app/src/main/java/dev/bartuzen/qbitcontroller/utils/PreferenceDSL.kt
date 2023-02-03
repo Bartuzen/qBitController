0e59258726e7700c29d0656b2b56a558af5d3fab
Bartu Özen <15032958+Bartuzen@users.noreply.github.com>
2023-02-03 16:41:13 +0300

    Use app strings for preference dialog buttons

--- app/src/main/java/dev/bartuzen/qbitcontroller/utils/PreferenceDSL.kt
+++ app/src/main/java/dev/bartuzen/qbitcontroller/utils/PreferenceDSL.kt
@@ -1,6 +1,7 @@
 package dev.bartuzen.qbitcontroller.utils
 
 import androidx.preference.CheckBoxPreference
+import androidx.preference.DialogPreference
 import androidx.preference.DropDownPreference
 import androidx.preference.EditTextPreference
 import androidx.preference.ListPreference
@@ -10,6 +11,7 @@ import androidx.preference.PreferenceCategory
 import androidx.preference.PreferenceFragmentCompat
 import androidx.preference.SeekBarPreference
 import androidx.preference.SwitchPreference
+import dev.bartuzen.qbitcontroller.R
 
 class PreferenceDSL(private val fragment: PreferenceFragmentCompat) {
     private val context = fragment.preferenceManager.context
@@ -28,19 +30,19 @@ class PreferenceDSL(private val fragment: PreferenceFragmentCompat) {
     }
 
     fun dropDown(block: DropDownPreference.() -> Unit) {
-        screen.addPreference(DropDownPreference(context).apply(block))
+        screen.addPreference(DropDownPreference(context).setDialogText().apply(block))
     }
 
     fun editText(block: EditTextPreference.() -> Unit) {
-        screen.addPreference(EditTextPreference(context).apply(block))
+        screen.addPreference(EditTextPreference(context).setDialogText().apply(block))
     }
 
     fun list(block: ListPreference.() -> Unit) {
-        screen.addPreference(ListPreference(context).apply(block))
+        screen.addPreference(ListPreference(context).setDialogText().apply(block))
     }
 
     fun multiSelectList(block: MultiSelectListPreference.() -> Unit) {
-        screen.addPreference(MultiSelectListPreference(context).apply(block))
+        screen.addPreference(MultiSelectListPreference(context).setDialogText().apply(block))
     }
 
     fun seekBar(block: SeekBarPreference.() -> Unit) {
@@ -49,6 +51,11 @@ class PreferenceDSL(private val fragment: PreferenceFragmentCompat) {
 
     fun switch(block: SwitchPreference.() -> Unit) {
         screen.addPreference(SwitchPreference(context).apply(block))
+    }
+
+    private fun <T : DialogPreference> T.setDialogText() = apply {
+        positiveButtonText = context.getString(R.string.dialog_ok)
+        negativeButtonText = context.getString(R.string.dialog_cancel)
     }
 
     fun build() {
