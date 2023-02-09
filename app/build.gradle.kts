a9248293958cd14561834eb7bdf09e3209f0bc36
Bartu Özen <15032958+Bartuzen@users.noreply.github.com>
2023-02-09 10:45:32 +0300

    Update AppCompat to version 1.6.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -102,7 +102,7 @@ if (isFirebaseEnabled) {
 
 dependencies {
     implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.appcompat:appcompat:1.6.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.8.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.recyclerview:recyclerview:1.2.1")
