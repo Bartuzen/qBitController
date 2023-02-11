41fa1dbf8b2971a0f0bd0a938b1b266a106e71ed
Bartu Özen <15032958+Bartuzen@users.noreply.github.com>
2023-02-11 12:34:41 +0300

    Remove unnecessary return

--- app/src/main/java/dev/bartuzen/qbitcontroller/model/deserializers/PeerFilesDeserializer.kt
+++ app/src/main/java/dev/bartuzen/qbitcontroller/model/deserializers/PeerFilesDeserializer.kt
@@ -6,6 +6,6 @@ import com.fasterxml.jackson.databind.JsonDeserializer
 
 class PeerFilesDeserializer : JsonDeserializer<List<String>>() {
     override fun deserialize(parser: JsonParser?, context: DeserializationContext?): List<String> {
-        return parser?.valueAsString.takeIf { it?.isNotEmpty() == true }?.split("\n") ?: return emptyList()
+        return parser?.valueAsString.takeIf { it?.isNotEmpty() == true }?.split("\n") ?: emptyList()
     }
 }
