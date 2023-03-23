29596345a63a18b9446f20a1f2f124423e6073f0
Bartu Özen <15032958+Bartuzen@users.noreply.github.com>
2023-03-23 18:33:38 +0300

    Update Fragment to version 1.5.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -116,7 +116,7 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
 
-    implementation("androidx.fragment:fragment-ktx:1.5.5")
+    implementation("androidx.fragment:fragment-ktx:1.5.6")
 
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.0")
 
