a3035fab245e53b5415abbf66bdc70977df38599
Bartu Özen <15032958+Bartuzen@users.noreply.github.com>
2023-03-26 16:35:22 +0300

    Request notification permission when app starts

--- app/src/main/java/dev/bartuzen/qbitcontroller/ui/main/MainActivity.kt
+++ app/src/main/java/dev/bartuzen/qbitcontroller/ui/main/MainActivity.kt
@@ -1,12 +1,15 @@
 package dev.bartuzen.qbitcontroller.ui.main
 
+import android.Manifest
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
 import androidx.activity.OnBackPressedCallback
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
@@ -49,6 +52,8 @@ class MainActivity : AppCompatActivity() {
             binding.layoutDrawer.close()
         }
     }
+
+    private val permissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) {}
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -142,6 +147,9 @@ class MainActivity : AppCompatActivity() {
 
             binding.textClickToAddServer.visibility = if (serverList.isEmpty()) View.VISIBLE else View.GONE
             addServerAdapter.isVisible = serverList.isEmpty()
+            if (serverList.isNotEmpty()) {
+                requestNotificationPermission()
+            }
         }
 
         if (savedInstanceState == null) {
@@ -228,5 +236,13 @@ class MainActivity : AppCompatActivity() {
         drawerAdapter.removeAdapter(adapter)
         binding.recyclerDrawer.adapter = null
         binding.recyclerDrawer.adapter = drawerAdapter
+    }
+
+    private fun requestNotificationPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (!shouldShowRequestPermissionRationale(Manifest.permission.POST_NOTIFICATIONS)) {
+                permissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+            }
+        }
     }
 }
