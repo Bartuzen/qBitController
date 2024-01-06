ed757ab52626b8ac208511e5e0f902b69a6bca0f
Bartu Özen <git@bartuzen.dev>
2024-01-07 02:39:42 +0300

    Version 0.8.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "dev.bartuzen.qbitcontroller"
         minSdk = 21
         targetSdk = 34
-        versionCode = 15
-        versionName = "0.8.2"
+        versionCode = 16
+        versionName = "0.8.3"
     }
 
     buildTypes {
