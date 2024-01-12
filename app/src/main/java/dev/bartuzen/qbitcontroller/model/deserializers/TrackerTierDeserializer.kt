e636f815afc806259a034fac3882f54afe7461be
Bartu Özen <git@bartuzen.dev>
2024-01-12 17:39:16 +0300

    Fix formatting

--- app/src/main/java/dev/bartuzen/qbitcontroller/model/deserializers/TrackerTierDeserializer.kt
+++ app/src/main/java/dev/bartuzen/qbitcontroller/model/deserializers/TrackerTierDeserializer.kt
@@ -5,7 +5,7 @@ import com.fasterxml.jackson.core.JsonToken
 import com.fasterxml.jackson.databind.DeserializationContext
 import com.fasterxml.jackson.databind.JsonDeserializer
 
-class TrackerTierDeserializer: JsonDeserializer<Int?>() {
+class TrackerTierDeserializer : JsonDeserializer<Int?>() {
     override fun deserialize(parser: JsonParser, context: DeserializationContext): Int? {
         return if (parser.currentToken() == JsonToken.VALUE_NUMBER_INT) {
             parser.intValue.takeIf { it != -1 }
