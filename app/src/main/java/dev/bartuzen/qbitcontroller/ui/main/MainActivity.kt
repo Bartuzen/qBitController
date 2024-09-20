2fcd5a5bb484e853320804ec3b7cd5fc227c7f9c
Bartu Özen <git@bartuzen.dev>
2024-09-20 18:08:40 +0300

    Update dependencies

--- app/src/main/java/dev/bartuzen/qbitcontroller/ui/main/MainActivity.kt
+++ app/src/main/java/dev/bartuzen/qbitcontroller/ui/main/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.semantics.testTagsAsResourceId
@@ -44,6 +45,7 @@ class MainActivity : AppCompatActivity() {
 
     private var navController: NavHostController? = null
 
+    @OptIn(ExperimentalComposeUiApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
