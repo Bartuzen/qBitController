853ff222b0352d82f4b10e0b032ffd2ce2cfca59
Bartu Özen <git@bartuzen.dev>
2025-04-03 17:50:08 +0300

    Bump Java version to 21

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -88,12 +88,12 @@ android {
     compileOptions {
         isCoreLibraryDesugaringEnabled = true
 
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
+        jvmTarget = JavaVersion.VERSION_21.toString()
 
         freeCompilerArgs = listOf(
             "-opt-in=androidx.compose.foundation.ExperimentalFoundationApi",
