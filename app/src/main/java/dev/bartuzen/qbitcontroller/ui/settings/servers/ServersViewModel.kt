a461d6563633e0b1d2bf8690bf53fdceb521c036
Bartu Özen <git@bartuzen.dev>
2025-05-09 00:25:55 +0300

    Fix animation issue in ServersScreen

--- app/src/main/java/dev/bartuzen/qbitcontroller/ui/settings/servers/ServersViewModel.kt
+++ app/src/main/java/dev/bartuzen/qbitcontroller/ui/settings/servers/ServersViewModel.kt
@@ -1,16 +1,10 @@
 package dev.bartuzen.qbitcontroller.ui.settings.servers
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import dev.bartuzen.qbitcontroller.data.ServerManager
-import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.stateIn
 
 class ServersViewModel(
     serverManager: ServerManager,
 ) : ViewModel() {
     val servers = serverManager.serversFlow
-        .map { it.values.toList() }
-        .stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000), emptyList())
 }
