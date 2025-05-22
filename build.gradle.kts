5f7337bd9c2addd3140049ed17790df494f8f33a
Bartu Özen <git@bartuzen.dev>
2025-05-22 03:38:38 +0300

    Remove unused parcelize plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,6 @@ plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.kotlinMultiplatform) apply false
     alias(libs.plugins.kotlinAndroid) apply false
-    alias(libs.plugins.kotlin.parcelize) apply false
     alias(libs.plugins.kotlinter) apply false
     alias(libs.plugins.kotlinxSerialization) apply false
     alias(libs.plugins.compose.compiler) apply false
