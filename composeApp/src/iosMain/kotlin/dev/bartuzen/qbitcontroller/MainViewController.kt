64e7e2d371988397a082ad78532b0dd320523705
Bartu Özen <git@bartuzen.dev>
2025-05-26 23:59:57 +0300

    Run ConfigMigrator on iOS

--- composeApp/src/iosMain/kotlin/dev/bartuzen/qbitcontroller/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/dev/bartuzen/qbitcontroller/MainViewController.kt
@@ -1,9 +1,15 @@
 package dev.bartuzen.qbitcontroller
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.window.ComposeUIViewController
+import dev.bartuzen.qbitcontroller.data.ConfigMigrator
 import dev.bartuzen.qbitcontroller.di.appModule
 import dev.bartuzen.qbitcontroller.ui.main.MainScreen
 import org.koin.compose.KoinApplication
+import org.koin.compose.koinInject
 
 @Suppress("ktlint:standard:function-naming")
 fun MainViewController() = ComposeUIViewController {
@@ -12,6 +18,13 @@ fun MainViewController() = ComposeUIViewController {
             modules(appModule)
         },
     ) {
+        val configMigrator = koinInject<ConfigMigrator>()
+        var ranConfigMigration by remember { mutableStateOf(false) }
+        if (!ranConfigMigration) {
+            configMigrator.run()
+            ranConfigMigration = true
+        }
+
         MainScreen()
     }
 }
