62eb2bab5d253be632c6766dca6dbfa795751ca9
Bartu Özen <git@bartuzen.dev>
2025-05-30 12:52:42 +0300

    Make EmptyListMessage scrollable

--- composeApp/src/commonMain/kotlin/dev/bartuzen/qbitcontroller/ui/components/EmptyListMessage.kt
+++ composeApp/src/commonMain/kotlin/dev/bartuzen/qbitcontroller/ui/components/EmptyListMessage.kt
@@ -7,6 +7,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -28,6 +30,7 @@ fun EmptyListMessage(
     Column(
         modifier = modifier
             .fillMaxSize()
+            .verticalScroll(rememberScrollState())
             .padding(16.dp),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
