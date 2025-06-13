22d9d38cbc1343083fe35f1cdf04ec4f8a1e7cca
Bartu Özen <git@bartuzen.dev>
2025-06-13 09:51:32 +0300

    Start Koin before Compose on desktop

--- composeApp/src/desktopMain/kotlin/dev/bartuzen/qbitcontroller/Main.kt
+++ composeApp/src/desktopMain/kotlin/dev/bartuzen/qbitcontroller/Main.kt
@@ -2,10 +2,6 @@ package dev.bartuzen.qbitcontroller
 
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
@@ -15,8 +11,7 @@ import dev.bartuzen.qbitcontroller.ui.main.MainScreen
 import dev.bartuzen.qbitcontroller.ui.theme.AppTheme
 import dev.bartuzen.qbitcontroller.utils.stringResource
 import org.jetbrains.compose.resources.painterResource
-import org.koin.compose.KoinApplication
-import org.koin.compose.koinInject
+import org.koin.core.context.startKoin
 import qbitcontroller.composeapp.generated.resources.Res
 import qbitcontroller.composeapp.generated.resources.app_name
 import qbitcontroller.composeapp.generated.resources.icon_rounded
@@ -25,32 +20,27 @@ import java.awt.Color
 fun main() {
     System.setProperty("apple.awt.application.appearance", "system")
 
+    val koinApplication = startKoin {
+        modules(appModule)
+    }
+    val koin = koinApplication.koin
+
+    val configMigrator = koin.get<ConfigMigrator>()
+    configMigrator.run()
+
     application {
         Window(
             onCloseRequest = ::exitApplication,
             title = stringResource(Res.string.app_name),
             icon = painterResource(Res.drawable.icon_rounded),
         ) {
-            KoinApplication(
-                application = {
-                    modules(appModule)
-                },
-            ) {
-                val configMigrator = koinInject<ConfigMigrator>()
-                var ranConfigMigration by remember { mutableStateOf(false) }
-                if (!ranConfigMigration) {
-                    configMigrator.run()
-                    ranConfigMigration = true
+            AppTheme {
+                val backgroundColor = MaterialTheme.colorScheme.background
+                LaunchedEffect(backgroundColor) {
+                    window.background = Color(backgroundColor.toArgb())
                 }
 
-                AppTheme {
-                    val backgroundColor = MaterialTheme.colorScheme.background
-                    LaunchedEffect(backgroundColor) {
-                        window.background = Color(backgroundColor.toArgb())
-                    }
-
-                    MainScreen()
-                }
+                MainScreen()
             }
         }
     }
