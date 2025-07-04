9e1b75b2098e8a4618b4454b9a37ebb66991ffef
Bartu Özen <git@bartuzen.dev>
2025-07-04 11:07:41 +0300

    Revert "Start Koin before Compose on iOS"
    This reverts commit f8b403deae17f991ff5cfe0cb97ba27a0cd51c13.
    Breaks multi-window on iPad.

--- composeApp/src/iosMain/kotlin/dev/bartuzen/qbitcontroller/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/dev/bartuzen/qbitcontroller/MainViewController.kt
@@ -1,23 +1,30 @@
 package dev.bartuzen.qbitcontroller
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.window.ComposeUIViewController
 import dev.bartuzen.qbitcontroller.data.ConfigMigrator
 import dev.bartuzen.qbitcontroller.di.appModule
 import dev.bartuzen.qbitcontroller.ui.main.MainScreen
-import org.koin.core.context.startKoin
-import platform.UIKit.UIViewController
+import org.koin.compose.KoinApplication
+import org.koin.compose.koinInject
 
 @Suppress("ktlint:standard:function-naming", "unused", "FunctionName")
-fun MainViewController(): UIViewController {
-    val koinApplication = startKoin {
-        modules(appModule)
-    }
-    val koin = koinApplication.koin
+fun MainViewController() = ComposeUIViewController {
+    KoinApplication(
+        application = {
+            modules(appModule)
+        },
+    ) {
+        val configMigrator = koinInject<ConfigMigrator>()
+        var ranConfigMigration by remember { mutableStateOf(false) }
+        if (!ranConfigMigration) {
+            configMigrator.run()
+            ranConfigMigration = true
+        }
 
-    val configMigrator = koin.get<ConfigMigrator>()
-    configMigrator.run()
-
-    return ComposeUIViewController {
         MainScreen()
     }
 }
