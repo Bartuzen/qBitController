b248f14a2a12e027a00c60c18aa8b3f335a7d7bf
Bartu Özen <git@bartuzen.dev>
2025-07-11 12:16:08 +0300

    Version 2.0.1

--- buildSrc/src/main/java/dev/bartuzen/qbitcontroller/Versions.kt
+++ buildSrc/src/main/java/dev/bartuzen/qbitcontroller/Versions.kt
@@ -3,8 +3,8 @@ package dev.bartuzen.qbitcontroller
 import org.gradle.api.JavaVersion as GradleJavaVersion
 
 object Versions {
-    const val AppVersion = "2.0.0"
-    const val AppVersionCode = 22
+    const val AppVersion = "2.0.1"
+    const val AppVersionCode = 23
 
     object Android {
         const val CompileSdk = 36
