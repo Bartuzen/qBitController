48be3288b5b74d2b196257a7973a52d246a5ed64
Bartu Özen <git@bartuzen.dev>
2025-08-15 13:19:04 +0300

    Use Base64 instead of URL encoding in  serializableNavType

--- composeApp/src/commonMain/kotlin/dev/bartuzen/qbitcontroller/utils/SerializableNavType.kt
+++ composeApp/src/commonMain/kotlin/dev/bartuzen/qbitcontroller/utils/SerializableNavType.kt
@@ -4,8 +4,8 @@ import androidx.navigation.NavType
 import androidx.savedstate.SavedState
 import androidx.savedstate.read
 import androidx.savedstate.write
-import io.ktor.http.decodeURLPart
-import io.ktor.http.encodeURLParameter
+import io.ktor.util.decodeBase64String
+import io.ktor.util.encodeBase64
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.serializer
 
@@ -14,19 +14,19 @@ inline fun <reified T : Any> serializableNavType(): NavType<T> {
     return object : NavType<T>(false) {
         override fun put(bundle: SavedState, key: String, value: T) {
             val json = Json.encodeToString(serializer, value)
-            bundle.write { putString(key, json.encodeURLParameter()) }
+            bundle.write { putString(key, json.encodeBase64()) }
         }
 
         override fun get(bundle: SavedState, key: String): T {
-            return Json.decodeFromString(serializer, bundle.read { getString(key) }.decodeURLPart())
+            return Json.decodeFromString(serializer, bundle.read { getString(key) }.decodeBase64String())
         }
 
         override fun parseValue(value: String): T {
-            return Json.decodeFromString(serializer, value.decodeURLPart())
+            return Json.decodeFromString(serializer, value.decodeBase64String())
         }
 
         override fun serializeAsValue(value: T): String {
-            return Json.encodeToString(serializer, value).encodeURLParameter()
+            return Json.encodeToString(serializer, value).encodeBase64()
         }
     }
 }
