ddf93567a5247a18432e17bbbcfe8ccaa28dcceb
Bartu Özen <git@bartuzen.dev>
2025-08-25 20:32:37 +0300

    Add support for multiple windows on Android

--- composeApp/src/androidMain/kotlin/dev/bartuzen/qbitcontroller/App.kt
+++ composeApp/src/androidMain/kotlin/dev/bartuzen/qbitcontroller/App.kt
@@ -2,6 +2,10 @@ package dev.bartuzen.qbitcontroller
 
 import android.app.Application
 import android.app.UiModeManager
+import android.content.Intent
+import android.content.pm.ShortcutInfo
+import android.content.pm.ShortcutManager
+import android.graphics.drawable.Icon
 import android.os.Build
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.content.getSystemService
@@ -11,15 +15,19 @@ import dev.bartuzen.qbitcontroller.data.SettingsManager
 import dev.bartuzen.qbitcontroller.data.Theme
 import dev.bartuzen.qbitcontroller.data.notification.AppNotificationManager
 import dev.bartuzen.qbitcontroller.di.appModule
+import dev.bartuzen.qbitcontroller.utils.getString
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import org.koin.android.ext.android.inject
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.androidx.workmanager.koin.workManagerFactory
 import org.koin.core.context.startKoin
+import qbitcontroller.composeapp.generated.resources.Res
+import qbitcontroller.composeapp.generated.resources.shortcut_new_window
 
 class App : Application() {
     override fun onCreate() {
@@ -67,6 +75,27 @@ class App : Application() {
                     AppCompatDelegate.setDefaultNightMode(mode)
                 }
             }
+        }
+
+        registerShortcuts()
+    }
+
+    private fun registerShortcuts() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val shortcutManager = getSystemService<ShortcutManager>() ?: return
+
+            val newWindowShortcut = ShortcutInfo.Builder(this, "new_window")
+                .setShortLabel(runBlocking { getString(Res.string.shortcut_new_window) })
+                .setIcon(Icon.createWithResource(this, R.drawable.sc_new_window))
+                .setIntent(
+                    Intent(this, MainActivity::class.java).apply {
+                        action = Intent.ACTION_VIEW
+                        addFlags(Intent.FLAG_ACTIVITY_NEW_DOCUMENT or Intent.FLAG_ACTIVITY_MULTIPLE_TASK)
+                    },
+                )
+                .build()
+
+            shortcutManager.dynamicShortcuts = listOf(newWindowShortcut)
         }
     }
 }
